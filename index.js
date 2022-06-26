55eb6cd62547859243d3856276ab422128566f4a
jake dapper <jakedapper@gmail.com>
2022-06-26 09:24:59 -0500

    commit tree 60562207b3f69267a28c442bab3c5694679d7fe0

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+   
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll'];
+
+// function wrapGifts(gifts) {
+//     for (let i=0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+let names = ["Charlie", "Samip", "Ali"]
+let eventName = 'birthday'
+
+function writeCards(names, eventName){
+    let cardMessages = [];
+    for (let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+        cardMessages.push(message);
+        debugger;
+    }
+    return cardMessages
+}
+
+console.log(writeCards(names, eventName));
+
+function countDown(number){
+    let i = 0;
+    while (i <= number){
+      console.log(number--);
+    }
+}
